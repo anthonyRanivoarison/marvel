4ff2d57b0f114fbca1347a0bd2675634c19dda1c
anthonyRanivoarison <antonitsiory@gmail.com>
2025-08-10 12:27:41 +0300

    fix: cors resolved

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import marvelRoutes from "./src/routes/marvelRoutes.js";
 import type { Request, Response} from "express"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello...This is the backend of marvel app")
