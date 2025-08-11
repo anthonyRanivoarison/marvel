4cc21b64d34162a02bd40249421808edb963477f
anthonyRanivoarison <antonitsiory@gmail.com>
2025-08-11 12:37:06 +0300

    feat: add to pages logic to fetch API

--- client/src/pages/CharacterFormPage.tsx
+++ client/src/pages/CharacterFormPage.tsx
@@ -1,6 +1,40 @@
+import {useState} from "react";
+import {Button} from "@/components/ui/button.tsx";
+import InputForm from "@/components/templates/InputForm.tsx";
+import {useQuery} from "@tanstack/react-query";
+import axios from "axios";
+
 const CharacterFormPage = () => {
-  return <div>
-    FormPage
+  const [name, setName] = useState('');
+  const [realName, setRealName] = useState('');
+  const [universe, setUniverse] = useState('');
+
+  const handleSubmit = (e: any): | void => {
+    e.preventDefault();
+    const {data: string, isLoading, isError} = useQuery({
+      queryKey: ["characters", name],
+      queryFn: async () => {
+        const response = await axios.post("http://localhost:8800/characters", {
+          name, realName, universe
+        });
+        return response.data;
+      }
+    })
+    if (isError) console.log("Error")
+    else if (isLoading) console.log("Loading");
+  }
+
+  return <div className="flex items-center w-full min-h-screen">
+    <div className="bg-blue-200 w-2/6 min-h-screen"></div>
+    <div className="p-8 space-y-3 w-4/6 bg-white min-h-screen">
+      <h2 className="text-3xl font-bold">Create your own marvel collector</h2>
+      <form onSubmit={handleSubmit} className="w-full space-y-2">
+        <InputForm state={name} setState={setName} label="Name" />
+        <InputForm state={realName} setState={setRealName} label="Real name" />
+        <InputForm state={universe} setState={setUniverse} label="Universe" />
+        <Button className="text-white px-4 py-2 bg-black">Submit</Button>
+      </form>
+    </div>
   </div>
 }
 
--- client/src/pages/CharacterPage.tsx
+++ client/src/pages/CharacterPage.tsx
@@ -1,11 +1,28 @@
 import {useParams} from "react-router-dom";
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
+import type {Character} from "@/components/templates/MarvelCard.tsx";
 
 const CharacterPage = () => {
   const params = useParams();
-
   const ID: string | undefined = params.id;
+  const {data: character, isError, isLoading} = useQuery<Character>({
+    queryKey: ["Marvel", "Character"],
+    queryFn: async () => {
+      const response = await axios.get(`http://localhost:8800/characters/${ID}`);
+      console.log(response.data)
+      return response.data
+    }
+  });
 
-  return <h1>Character page {ID}</h1>
+  if (isLoading) return <p>Loading...</p>;
+  if (isError || !character) return <p>Error loading characters</p>;
+
+  return <div>
+    <h1 className="text-black text-5xl">{character.name}</h1>
+    <span>{character.realName}</span>
+    <p>{character.universe}</p>
+  </div>
 }
 
 export default CharacterPage;
